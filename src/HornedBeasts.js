98e126e5d71256731ebb5432c27e8ba2eda23a99
Terransky <nodeathking@gmail.com>
2021-03-24 23:47:57 -0700

    where did I go wrong with my life

--- src/HornedBeasts.js
+++ src/HornedBeasts.js
@@ -1,13 +1,16 @@
+
 import React from 'react';
 import './HornedBeasts.css';
+import data from './data.json'; //an array of objects with properties: image_url, title, description, keyword, horns
 
+console.log(data.map(beast => beast.name));
 
 class HornedBeasts extends React.Component {
   render() {
     return (
       <div>
-        <h2>{this.props.name}</h2>
-        <img title={this.props.name} alt={this.props.name + ' image'} src={this.props.imgUrl}></img>
+        <h2>this.props.name</h2>
+        <img title={this.props.title} alt={this.props.title + ' image'} src={this.props.imgUrl}></img>
         <p>{this.props.description}</p>
       </div>
     );
