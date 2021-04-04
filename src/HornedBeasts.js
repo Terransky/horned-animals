9f1d405f888d84df94abc9f26209853f6085c697
Anthony Turinski <nodeathking@gmail.com>
2021-04-04 13:56:33 -0700

    lab02 mostly complete

--- src/HornedBeasts.js
+++ src/HornedBeasts.js
@@ -1,14 +1,46 @@
 import React from 'react';
-import './HornedBeasts.css';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import { AiFillHeart } from 'react-icons/ai';
 
 
 class HornedBeasts extends React.Component {
+  constructor(props) { //this lets us create an object in React
+    super(props);
+    this.state = {
+      numberOfHearts: 0,
+    }
+  }
+
+  buttonClicked = () => {
+    this.setState({ //this changes the state in JSX
+      numberOfHearts: this.state.numberOfHearts + 1,
+    });
+  }
+
   render() {
     return (
       <div>
-        <h2>{this.props.name}</h2>
-        <img title={this.props.name} alt={this.props.name + ' image'} src={this.props.imgUrl}></img>
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src={this.props.image_url} style={{ width: '100%' }}/>
+            <Card.Body>
+            <Card.Title>{this.props.title}</Card.Title>
+            <Card.Text>
+              {this.props.description}
+              <div><AiFillHeart />{this.state.numberOfHearts}</div>
+            </Card.Text>
+            <Button onClick={this.buttonClicked} variant="secondary" size="lg block">
+              This is my favorite Horned Beast!
+            </Button>
+            </Card.Body>
+        </Card>
+        {/* <h2>{this.props.title}</h2>
+        <img onClick={this.buttonClicked} className="animal-pictures" src={this.props.image_url} alt={this.props.keyword} title={this.props.title}/> */}
+        <div><AiFillHeart />{this.state.numberOfHearts}</div>
         <p>{this.props.description}</p>
+        <Button onClick={this.buttonClicked} variant="secondary" size="lg block">
+          This is my favorite Horned Beast!
+        </Button>
       </div>
     );
   }
--- src/Main.js
+++ src/Main.js
@@ -3,20 +3,33 @@
 // The Main component needs to pass title, imageUrl, and description into each HornedBeast component. For the purpose of today’s lab, you can pass whatever title, url and description that you want into each HornedBeast commponent.
 
 // The HornedBeast component needs to contain an <h2> that displays the title of the animal, an <img> element with src, alt and title attributes, and a <p> that displays the description.
+
+//The Main component should pass props for the title, image and description to each HornedBeast component. You will find this information in the provided JSON file.
+
 import React from 'react';
 import HornedBeasts from './HornedBeasts';
 import './Main.css';
 
-// import CardColumns from 'react-bootstrap/CardDeck';
+import CardColumns from 'react-bootstrap/CardDeck';
 
 import data from './data.json';
 
 class Main extends React.Component {
   render() {
+    let beastArray = data.map( animal => { //here we create an array of objects from data.json and assign the properties
+      return <HornedBeasts // this passes the json props to the HornedBeast component (like a template) and assigns it to the array
+        name = {animal.keyword}
+        title = {animal.title}
+        image_url = {animal.image_url}
+        description = {animal.description}
+        />
+    });
+    
     return (
-    <main>
-      <HornedBeasts name= {data[0].title} title='whatever' imgUrl='https://placehold.it/400x300/444' description='whatever' />
-      <HornedBeasts name='3' title='whatever' imgUrl='https://placehold.it/400x300/444' description='whatever' />
+      <main>
+      <CardColumns>
+        {beastArray} {/*this displays the whole array of components we made earlier*/}
+      </CardColumns>
     </main>
     );
   }
